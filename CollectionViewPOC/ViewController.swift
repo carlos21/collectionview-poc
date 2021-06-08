e9289b70f2d3eb21e22b41a629c76ec766f9acb2
Carlos Duclos <darkzeratul64@gmail.com>
2021-06-08 10:56:46 -0500

    Initial Commit

---
+++ CollectionViewPOC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewPOC
+//
+//  Created by Carlos Duclos on 8/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
